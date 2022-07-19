8edcb2e53f4c5d2dbe99509509e33d67d919df7c
pouiee <jonathan.jasso5@gmail.com>
2022-07-19 14:13:44 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
+from dotenv import load_dotenv
+
+# used for method 2
+load_dotenv()
 
 def my_keys():
-    id= os.environ.get('SECRET_ID')
-    password= os.environ.get("SECRET_PASSWORD")
+    # method 1
+    # id= os.environ.get('SECRET_ID')
+    # password= os.environ.get("SECRET_PASSWORD")
+
+    # method 2
+    id = os.getenv('SECRET_ID')
+    password = os.getenv('SECRET_PASSWORD')
 
     print(id,password)
 
